a4332b530e5cce2ebb4890a421b817d0c527a01a
Derek Boat <derek.boat@gmail.com>
2020-01-31 16:43:37 -0600

    set up of files is good

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+//Create an application that records employee salaries and adds salaries up to report monthly costs.
+
+//The application should have an input form that collects employee first name, last name, ID number, job title, annual salary.
+$( document ).ready( onReady );
+
+function onReady(){
+console.log('jq works');
+}
+console.log('hello');
